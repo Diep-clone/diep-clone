836c3042b6cc075d4f293eb0bff25a8f1fa17911
ybs1164 <ybs1164@gmail.com>
2020-04-14 17:37:07 +0900

    30

--- main.go
+++ main.go
@@ -169,7 +169,7 @@ func sendUpdates(ticker time.Ticker) {
 				H: 720 / u.Camera.Z * 2,
 			})
 			//log.Println(objList)
-			var visibleObject = make([]map[string]interface{}, 0, 50) // 50 개 미리 활당
+			var visibleObject = make([]map[string]interface{}, 0, 30) // 30 개 미리 활당
 
 			for _, o := range objList {
 				if o.X < u.Camera.Pos.X+1280/u.Camera.Z+o.R &&
